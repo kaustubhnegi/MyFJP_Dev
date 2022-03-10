1cfa70e3a911727f45032c1ccf868906b580881e
kaustubhnegi <kaustubhnegi1@gmail.com>
2022-03-10 22:13:15 +0530

    path

---
+++ path.js
@@ -0,0 +1,14 @@
+//"C:\Users\negir\Desktop\FJP6\module2\javascript\nodejs\path.js"
+
+let path = require('path');
+
+//console.log(path);
+
+// let extensionName = path.extname("C:\Users\negir\Desktop\FJP6\module2\javascript\nodejs\path.js");
+// //console.log(extensionName);
+
+// let baseName = path.basename("C:\Users\negir\Desktop\FJP6\module2\javascript\nodejs\path.js");
+// console.log(baseName);
+
+console.log(__dirname);
+console.log(__filename);
