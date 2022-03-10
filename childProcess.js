0f89e04fa85bef4881d1796ff90deb004839b2a1
kaustubhnegi <kaustubhnegi1@gmail.com>
2022-03-10 21:48:39 +0530

    child_process

---
+++ childProcess.js
@@ -0,0 +1,9 @@
+let cp = require('child_process');
+// console.log(cp);
+
+// cp.execFileSync("calc");
+//cp.execFileSync("notepad");
+//  cp.execSync('code');
+
+let content = cp.execSync("node test.js");
+console.log("Output of test.js file: "+content);
