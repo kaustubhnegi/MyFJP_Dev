01753361a166a6d822080a9a6350ff6a3838aea6
kaustubhnegi <kaustubhnegi1@gmail.com>
2022-03-10 22:48:25 +0530

    fs.js

--- fs.js
+++ fs.js
@@ -1,4 +1,13 @@
 let fs = require("fs");
-console.log(fs);
+//console.log(fs);
 
+let path = require("path");
+
+
+let filePath = path.join(__dirname,"file.txt");
+console.log(filePath);
+
+
+
+fs.writeFileSync(filePath,"Hello I am a text file");              // filepath along with filename
 
