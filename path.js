697adf62e90a7933c73a84046e8b97db3b330fe0
kaustubhnegi <kaustubhnegi1@gmail.com>
2022-03-10 22:23:49 +0530

    path

--- path.js
+++ path.js
@@ -10,5 +10,12 @@ let path = require('path');
 // let baseName = path.basename("C:\Users\negir\Desktop\FJP6\module2\javascript\nodejs\path.js");
 // console.log(baseName);
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);     //   returns path directory
+// console.log(__filename);    //   returns file directory
+
+let dirPath = __dirname;
+
+let newFilePath = path.join(dirPath,"test.js");
+console.log(newFilePath);
+
+    
